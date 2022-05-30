4aaf204d23878d099d21b759894eb2954dc241c0
HS Bilgen <sungurbilgen@gmail.com>
2022-05-31 00:47:26 +0300

    middleware updated

--- app/api/middleware/index.js
+++ app/api/middleware/index.js
@@ -1,23 +1,22 @@
 const errorHandler = (err, req, res, next) => {
-    app.use((err, req, res, next) => {
-        res.status(err.status || 500).json({
-            success: false,
-            message: err.message,
-            errors: err.errors
-        });
-    });
 	if (typeof err !== "object") {
 		// If the object is not an Error, create a representation that appears to be
 		err = {
+			success: false,
 			message: String(err)
 		};
 	} else {
 		// Ensure that err.message is enumerable (It is not by default)
+		err['success'] = err.status >= 400 || err.statusCode >=400 ? false : true; 
 		Object.defineProperty(err, "message", { enumerable: true });
 	}
 
 	res.set("Content-Type", "application/json");
-	res.status(err.status || err.statusCode || 500).send(JSON.stringify(err));
+	res.status(err.status || err.statusCode || 500).json({
+		success: false,
+		message: err.message,
+		errors: err.errors
+	});
 };
 
 module.exports = {
